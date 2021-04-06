657e5280653e8f7053a42f2e71e4287c569cb29e
Damián Ricobelli <damian.ricobelli@outlook.es>
2021-04-06 18:46:43 -0300

    Add config for ssl

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   future: {
     webpack5: true
+  },
+  async headers() {
+    return [
+      {
+        source: "/.well-known/pki-validation/AB59D8989CBE68FAFC42C3509C6704D2",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "text/html"
+          }
+        ]
+      }
+    ]
   }
 }
