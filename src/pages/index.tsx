0cd5b695ae4b6f5256326db04e3acbbfdf8afd21
Damián Ricobelli <damian.ricobelli@outlook.es>
2021-04-06 14:51:57 -0300

    Add GTM config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head"
-import { useState } from "react"
+import Router from "next/router"
+import { useState, useEffect } from "react"
 import { useScrollPosition } from "@n8tb1t/use-scroll-position"
 import Hero from "@components/ui/Hero"
 import Testimonial from "@components/ui/Testimonial"
@@ -7,9 +8,11 @@ import Use from "@components/ui/Use"
 import Footer from "@components/common/Footer"
 import { WhatsappButton } from "@components/ui/WhatsappButton"
 import React from "react"
+import { GTMPageView } from "@utils/gtm"
 
 export default function Home() {
   const [hideOnScroll, setHideOnScroll] = useState(false)
+
   useScrollPosition(
     ({ currPos }) => {
       setHideOnScroll(currPos.y < -600)
@@ -19,6 +22,15 @@ export default function Home() {
     false,
     300
   )
+
+  // Initiate GTM
+  useEffect(() => {
+    const handleRouteChange = (url: string) => GTMPageView(url)
+    Router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [])
 
   return (
     <>
